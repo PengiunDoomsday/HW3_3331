93aa382520a6b8dbd966a7a896887f834235a288
PengiunDoomsday <43393396+PengiunDoomsday@users.noreply.github.com>
2019-04-22 00:33:15 -0600

    Update ItemRenderer.java
    changes the color when selected

--- ItemRenderer.java
+++ ItemRenderer.java
@@ -4,11 +4,24 @@ import javax.swing.*;
 
 public class ItemRenderer extends JLabel implements ListCellRenderer<Item>{
 
+    public ItemRenderer() {
+        setOpaque(true);
+    }
+    
 	@Override
 	public Component getListCellRendererComponent(JList<? extends Item> list, Item item, int index, boolean isSelected,
 			boolean cellHasFocus) {
 		// TODO Auto-generated method stub
 		ItemView itemView = new ItemView(item);
+       
+		
+		if (isSelected) {
+            setBackground(list.getSelectionBackground());
+            setForeground(list.getSelectionForeground());
+        } else {
+            setBackground(list.getBackground());
+            setForeground(list.getForeground());
+        }
 		return itemView;
 	}
 	
