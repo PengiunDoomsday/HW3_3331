98924607182683ddcd6c6265be9aebb26226a128
PengiunDoomsday <43393396+PengiunDoomsday@users.noreply.github.com>
2019-04-22 02:53:44 -0600

    Update Main.java
    merged the toolbar class with the main

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.*;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 /**
  * A dialog for tracking the price of an item.
@@ -20,7 +21,7 @@ public class Main extends JFrame {
 
 	/** Special panel to display the watched item. */
 	private ItemView itemView;
-
+//	private Toolbar toolBar;
 	/** Message bar to display various messages. */
 	private JLabel msgBar = new JLabel(" ");
 	private Item item;
@@ -32,7 +33,6 @@ public class Main extends JFrame {
 	/**
 	 * Create a new dialog.
 	 * 
-	 * @throws IOException
 	 */
 	public Main() {
 		this(DEFAULT_SIZE);
@@ -41,7 +41,6 @@ public class Main extends JFrame {
 	/**
 	 * Create a new dialog of the given screen dimension.
 	 * 
-	 * @throws IOException
 	 */
 	public Main(Dimension dim) {
 		super("Price Watcher");
@@ -68,6 +67,9 @@ public class Main extends JFrame {
 		itemList = new JList<>(listModel);
 
 		configureUI();
+		JToolBarUI();
+		
+		
 
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setVisible(true);
@@ -113,8 +115,10 @@ public class Main extends JFrame {
 
 	/** Configure UI. */
 	private void configureUI() {
-		setLayout(new BorderLayout());
+		setLayout(new BorderLayout());		
+		
 		JPanel control = makeControlPanel();
+		JPanel toolbar = JToolBarUI();
 		control.setBorder(BorderFactory.createEmptyBorder(10, 16, 0, 16));
 		add(control, BorderLayout.NORTH);
 		JPanel board = new JPanel();
@@ -123,19 +127,74 @@ public class Main extends JFrame {
 		board.setLayout(new GridLayout(1, 1));
 //		itemView = new ItemView(item);
 //
-		itemView.setClickListener(this::viewPageClicked);
+//		itemView.setClickListener(this::viewPageClicked);
 //		board.add(itemView);
 		add(new JScrollPane(itemList));
 		board.add(itemList);
 		itemList.setCellRenderer(new ItemRenderer());
 		
+		getContentPane().add(toolbar, BorderLayout.NORTH);
+		
 		add(board, BorderLayout.CENTER);
 		msgBar.setBorder(BorderFactory.createEmptyBorder(10, 16, 10, 0));
 		add(msgBar, BorderLayout.SOUTH);
 	}
+	
+	
+	ImageIcon openIcon = new ImageIcon(Main.class.getResource("/image/blue question.png"));
+	ImageIcon saveIcon = new ImageIcon(Main.class.getResource("/image/ebay.png"));
+	ImageIcon newIcon = new ImageIcon(Main.class.getResource("/image/blue settings.png"));
+	
+	Action openAction = new AbstractAction("About", openIcon) {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JOptionPane.showMessageDialog(null, "PriceWatcher, version 13.1");
+			System.out.println("Opening About");
+		}
+	};
+	Action saveAction = new AbstractAction("Save", saveIcon) {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Save File");
+		}
+	};
+	Action newAction = new AbstractAction("New", newIcon) {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("New File");
+		}
+	};
+	
+	private JPanel JToolBarUI() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JMenuItem openMenuItem = new JMenuItem(openAction);
+		JMenuItem saveMenuItem = new JMenuItem(saveAction);
+		JMenuItem newMenuItem = new JMenuItem(newAction);
+
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.add(openMenuItem);
+		fileMenu.add(saveMenuItem);
+		fileMenu.add(newMenuItem);
+		menuBar.add(fileMenu);
+
+		JToolBar toolBar = new JToolBar();
+		panel.add(toolBar);
+		
+		toolBar.add(Box.createHorizontalGlue());
+		toolBar.setBorder(new LineBorder(Color.LIGHT_GRAY, 1));
+		toolBar.add(newAction);
+		toolBar.add(openAction);
+		toolBar.add(saveAction);
+		
+		return panel;
+	}
 
 	/** Create a control panel consisting of a refresh button. */
-	private JPanel makeControlPanel() {
+	private JPanel makeControlPanel() {		
+		
 		JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEADING));
 		JButton refreshButton = new JButton("Refresh Check prices");
 		refreshButton.setFocusPainted(false);
