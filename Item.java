ff064c74cc8dd8a2a4c81699e97f7692425ef815
PengiunDoomsday <43393396+PengiunDoomsday@users.noreply.github.com>
2019-05-08 09:30:17 -0600

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,28 +1,85 @@
 package pricewatcher.base;
 
-import java.awt.Component;
-import javax.swing.*;
+import java.io.*;
 
-public class ItemRenderer extends JLabel implements ListCellRenderer<Item> {
+public class Item {
+	/** Name of the item, use a good one, saved as a string */
+	private String name;
+	/** URL of the item, saved as a string */
+	private String url;
+	/** initial Price of the item, saved as a double */
+	private double initialPrice;
+	private double currentPrice;
+	private double priceChange;
+	private String dateAdded;
 
-	public ItemRenderer() {
-		setOpaque(true);
+	public Item next;
+
+	public Item(String name, String url, double currentPrice, double priceChange) throws IOException {
+		// contains name, url, current price
+		this.name = name;
+		this.url = url;
+		this.currentPrice = currentPrice;
+		this.priceChange = priceChange;
 	}
 
-	@Override
-	public Component getListCellRendererComponent(JList<? extends Item> list, Item item, int index, boolean isSelected,
-			boolean cellHasFocus) {
-		// TODO Auto-generated method stub
-		ItemView itemView = new ItemView(item);
-
-		if (isSelected) {
-			itemView.setBackground(list.getSelectionBackground());
-			itemView.setForeground(list.getSelectionForeground());
-		} else {
-			itemView.setBackground(list.getBackground());
-			itemView.setForeground(list.getForeground());
-		}
-		return itemView;
+	public Item() {
+		// TODO Auto-generated constructor stub
 	}
+
+	public Item(Item item) {
+		// TODO Auto-generated constructor stub
+	}
+
+	// Setter
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public void setInitialPrice(double initialPrice) {
+		this.initialPrice = initialPrice;
+	}
+
+	public void setCurrentPrice(double currentPrice) {
+		this.currentPrice = currentPrice;
+	}
+
+	public void setDateAdded(String dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+
+	public void setPriceChange(double priceChange) {
+		this.priceChange = priceChange;
+	}
+
+	// Getters
+	public String getName() {
+		return name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public double getInitialPrice() {
+		return initialPrice;
+	}
+
+	public double getCurrentPrice() {
+		return currentPrice;
+	}
+
+	public String getDateAdded() {
+		return dateAdded;
+	}
+
+	public double getPriceChange() {
+		return priceChange;
+	}
+
 
 }
