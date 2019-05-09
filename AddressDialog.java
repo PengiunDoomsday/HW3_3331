b69e6c7159e7b6bf643825a1b1621ecae9e20e33
PengiunDoomsday <43393396+PengiunDoomsday@users.noreply.github.com>
2019-05-09 10:33:34 -0600

    Update AddressDialog.java

--- AddressDialog.java
+++ AddressDialog.java
@@ -1,60 +1,50 @@
 package pricewatcher.base;
 
+import java.awt.*;
 
-import java.awt.Frame;
-import java.awt.GridLayout;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.*;
 
-public class AddressDialog extends JDialog {
 
-    JLabel Name = new JLabel("Name: ");
 
-    JLabel uRL = new JLabel("URL:");
+@SuppressWarnings("serial")
+public class AddDialog extends JDialog {
+	JLabel Name = new JLabel("Name: ");
+	JLabel uRL = new JLabel("URL:");
+	JLabel Price = new JLabel("Price:");
 
-    JLabel Price = new JLabel("Price:");
 
-    JTextField nameField = new JTextField();
 
-    JTextField urlField = new JTextField();
+	JTextField nameField = new JTextField();
+	JTextField urlField = new JTextField();
+	JTextField priceField = new JTextField("0.00");
+	JButton ok = new JButton("Ok");
 
-    JTextField priceField = new JTextField();
 
-    String[] list = new String[3];
+	String[] list = new String[3];
+      
+	
+	public AddDialog(AbstractAction abstractAction, boolean modal) {
+		super();
+		init();
+	}
 
-    /**
-     *
-     * @param owner
-     * @param modal
-     */
-    public AddressDialog(Frame owner, boolean modal) {
-        super(owner, modal);
-        init();
-    }
+	private void init() {
+		this.setTitle("Add");
+		this.setLayout(new GridLayout(4, 2));
+		this.add(Name);
+		this.add(nameField);
+		this.add(uRL);
+		this.add(urlField);
+		this.add(Price);
+		this.add(priceField);
+		this.add(ok);
+	}
 
-    /**
-     *
-     */
-    private void init() {
-        this.setTitle("Add");
-        this.setLayout(new GridLayout(4, 2));
-        this.add(Name);
-        this.add(nameField);
-        this.add(uRL);
-        this.add(urlField);
-        this.add(Price);
-        this.add(priceField);
-    }
-
-    /**
-     *
-     * @return
-     */
-    public String[] getAddress() {
-        list[0] = nameField.getText();
-        list[1] = urlField.getText();
-        list[2] = priceField.getText();
-        return list;
-    }
+	public String[] getAddress() {
+		list[0] = nameField.getText();
+		list[1] = urlField.getText();
+		list[2] = priceField.getText();
+		return list;
+	}
 }
+
